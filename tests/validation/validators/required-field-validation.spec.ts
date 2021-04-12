87dc639861ebc908f9ca441fcf2f3549f20de2fd
Brandon <brandon.amaral9658@gmail.com>
2021-04-12 15:10:38 -0300

    test: ensure ValidationComposite returns an error if validation fails

--- tests/validation/validators/required-field-validation.spec.ts
+++ tests/validation/validators/required-field-validation.spec.ts
@@ -10,15 +10,15 @@ const makeSut = (): RequiredFieldValidation => {
 };
 
 describe('RequiredField Validation', () => {
-  it('Should return a MissingParamError if validation fails', async () => {
+  it('Should return a MissingParamError if validation fails', () => {
     const sut = makeSut();
-    const error = await sut.validate({ invalid: faker.random.word() });
+    const error = sut.validate({ invalid: faker.random.word() });
     expect(error).toEqual(new MissingParamError(field));
   });
 
-  it('Should not return if validation succeeds', async () => {
+  it('Should not return if validation succeeds', () => {
     const sut = makeSut();
-    const error = await sut.validate({ [field]: faker.random.word() });
+    const error = sut.validate({ [field]: faker.random.word() });
     expect(error).toBeFalsy();
   });
 });
